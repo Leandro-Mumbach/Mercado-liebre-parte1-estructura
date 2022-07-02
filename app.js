f7ec81385c32d5d43074fc88e16e9aafbf9feed3
Leandro-Mumbach <lean21mumbach@gmail.com>
2022-07-02 12:48:37 -0300

    add cambios estilos

--- app.js
+++ app.js
@@ -4,5 +4,4 @@ const path= require("path")
 
 app.use(express.static("public"))
 app.get("/", (req, res) => {res.sendFile (path.join (__dirname, "views","home.html"))})
-app.get("/ofertas", (req, res) => {res.sendFile (path.join (__dirname, "views","ofertas.html"))})
 app.listen (3030, () => console.log("Servidor corriendo"))
